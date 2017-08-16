3c258849af66adcef6bf7a4ab7e03ac6f8c834f2
Matt <eclecticappdevelopment@gmail.com>
2017-08-16 21:35:05 +0100

    Fixes #3 to store to WP database
    Src: https://github.com/eclectic-matt/wordpress/issues/3
    
    Uses get_option('checklist-item') to store the array of items in the database, passed as $value to the edit form (also being updated).

--- plugins/pre-publish-checklist/pre-publish-checklist.php
+++ plugins/pre-publish-checklist/pre-publish-checklist.php
@@ -1,19 +1,24 @@
 <?php
 /**
  * @package Pre-Publish Checklist
- * @version 0.1
+ * @version 0.2
  */
 /*
 Plugin Name: Pre-Publish Checklist
 Plugin URI: http://wordpress.org/plugins/pre-publish-checklist/
 Description: This plugin adds a checklist into the Wordpress post editor, and once the author has checked that the requirements for the post have been met, the publish button will become available.
-Author: Matt Tiernan
-Version: 0.1
+Author: Matt Tiernan, Christien Guy
+Version: 0.2
 Author URI: http://eclecticapp.xyz
 */
 
+$check_defaults = ["Featured image is 740 x 240","Keyword has been set","Tags and categories set"];
+add_option('checklist-item', $check_defaults);
+
 // Initialisation function which loads stylesheets and scripts separately.
-add_action( 'admin_init', 'eclecticapp_init' );
+add_action('admin_menu', 'eclecticapp_checklist_settings_create');
+add_action('admin_init', 'eclecticapp_init');
+add_action('post_submitbox_misc_actions', 'eclecticapp_pre_publish_checklist_generate');
 
 function eclecticapp_init() {
 	wp_register_style( 'eclecticapp-list-styles', plugins_url('public/css/checklist.css', __FILE__) );
@@ -21,7 +26,7 @@ function eclecticapp_init() {
 
 	wp_register_script( 'eclecticapp-list-js', plugins_url('public/js/checklist-watch.js', __FILE__) );
 	$vars = array(
-		'totalChecklistItems' => count($GLOBALS['checklist_entries'])
+		'totalChecklistItems' => count(get_option('checklist-item',FALSE) )
 	);
 	wp_localize_script( 'eclecticapp-list-js', 'vars', $vars );
 	wp_enqueue_script('eclecticapp-list-js');
@@ -30,33 +35,47 @@ function eclecticapp_init() {
 	wp_enqueue_script('eclecticapp-edit-list-js');
 }
 
+// Sets up the form using separated parameters
+function eclecticapp_checklist_settings_create() {
+    $page_title = 'Checklist Admin';
+    $menu_title = 'Checklist Admin';
+    $capability = 'manage_options';
+    $menu_slug = 'checklist_settings';
+    $function = 'eclecticapp_checklist_settings_display';
+    $icon_url = '';
+    $position = 20;
 
-include_once('pre-publish-checklist-update.php');
-//include_once('pre-publish-checklist-menu.php');
-
-// Customising checklist
-$check_defaults = ["Featured Image Set", "Longtail keyword set", "Yoast SEO - Green"];
-if ( !isset( $GLOBALS['checklist_entries'] ) ){
-	$GLOBALS['checklist_entries'] = $check_defaults;
-}
-add_action('post_submitbox_misc_actions', 'eclecticapp_pre_publish_checklist_generate');
-
-//Testing menu page
-add_action('admin_menu','eclecticapp_pre_publish_checklist_add_menu');
-
-function eclecticapp_pre_publish_checklist_add_menu(){
-	add_menu_page("Publish Checklist","Checklist", 4, "checklist", "eclecticapp_pre_publish_checklist_add_settings_menu");
+    add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
 }
 
-function eclecticapp_pre_publish_checklist_add_settings_menu(){
-	global $num_ppc_checks;
-	$num_ppc_checks = count($GLOBALS['checklist_entries']);
+// Displays the form page
+function eclecticapp_checklist_settings_display() {
 	
+	// If the user is not able to manage options, they cannot see the settings form
+    if (!current_user_can('manage_options')) {
+        wp_die('Unauthorized user');
+    }
+	
+	// If the form nonce is invalid, quit the form as well - not working
+    /*if (!wp_verify_nonce( '_wp_nonce', 'ea_checklist_settings_action' )) {
+        wp_die('Nonce verification failed');
+    }*/
+	
+	// If there is POST data with updated entries, use these
+    if (isset($_POST['checklist-item'])) {
+        update_option('checklist-item', $_POST['checklist-item']);
+        $value = $_POST['checklist-item']; 
+    }
+	
+    $value = get_option('checklist-item', $check_defaults);
+	$num_ppc_checks = count($value);
+    
 	include 'public/partials/edit-checklist.php';
 }
 
+
 function eclecticapp_pre_publish_checklist_generate($post) {
-	$num_ppc_checks = count($GLOBALS['checklist_entries']);
+	$num_ppc_checks = count(get_option('checklist-item',FALSE));
 	include 'public/partials/post-edit-checklist.php';
 }
 

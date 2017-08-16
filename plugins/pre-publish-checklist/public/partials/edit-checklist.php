294546379b5764aa7a0b64dd9d7508bfba246f5f
Matt <eclecticappdevelopment@gmail.com>
2017-08-16 21:38:27 +0100

    Changes to Fix #3 and store/update in DB
    Changes to use $value as a storage array for the checklist items.
    
    Passes back to self with the POST values, which get picked up and stored by set_option('checklist-item').
    
    No changes to DOM/classes so all associated js/css works fine.

--- plugins/pre-publish-checklist/public/partials/edit-checklist.php
+++ plugins/pre-publish-checklist/public/partials/edit-checklist.php
@@ -1,11 +1,24 @@
-<h2>Checklist Editor ( <?php echo $num_ppc_checks ?> checks )</h2>
-<form action='<?php echo plugins_url('pre-publish-checklist-update.php', __FILE__); ?>' method='post' id='cl_update'>
-  <ul id="checklist-list">
-    <?php for ($i = 1; $i <= $num_ppc_checks; $i++): ?>
-      <li><input type="text" name="checklist-item[]" value="<?php echo $GLOBALS['checklist_entries'][$i - 1]; ?>"><button class="delete-entry-button" value="delete">Delete</button></li>
-    <?php endfor; ?>
-  </ul>
-  <button id="add-entry-button">Add</button>
+<?php
+
+$num_ppc_checks = count($value);
+echo '<h1>Checklist Settings (' . $num_ppc_checks . ' checks)</h1>
+	<form method="POST">
+		<ul id="checklist-list">';
+
+for ($i = 0; $i < $num_ppc_checks; $i++){
+	echo '<li>
+			<input type="text" name="checklist-item[]" value="' . $value[$i] . '">
+			<button class="delete-entry-button" value="delete">Delete</button>
+		</li>';
+}
+echo '
+</ul>
+  <button id="add-entry-button">Add</button>';
   
- <input type='submit' value='submit'>
-</form>
+  //echo wp_nonce_field( 'ea_checklist_settings_action' );
+  
+  echo '
+<input type="submit" value="Save" class="button button-primary button-large">
+</form>';
+ 
+?>

009dd57140226c2177258fa689fe1240e0de42fd
Matt <eclecticappdevelopment@gmail.com>
2017-08-16 21:49:04 +0100

    Amended to use new get_option values

--- plugins/pre-publish-checklist/public/partials/post-edit-checklist.php
+++ plugins/pre-publish-checklist/public/partials/post-edit-checklist.php
@@ -6,10 +6,11 @@
 		<em>All checks must be ticked before publishing!</em>
 		<br>
 		<div>
-      <?php for ($i = 1; $i <= $num_ppc_checks; $i++): ?>
+
+		<?php for ($i = 1; $i <= $num_ppc_checks; $i++): ?>
         <label class='selectit' for='ppc-check-<?php echo $i ?>'>
         <input type='checkbox' id='ppc-check-<?php echo $i ?>' onchange='ppc_check_ready()'></input>
-            Hello <?php $i . ": " . $GLOBALS['checklist_entries'][$i - 1]; //$check_titles[$i - 1]; ?>
+            <?php echo $i . ": " . $value[$i - 1]; ?>
         </label>
         <br>
       <?php endfor; ?>
